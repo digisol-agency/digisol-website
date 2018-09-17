4d6148691fbdb39802f837962bc1b2b51ba080a0
marzique <marzique@gmail.com>
2018-09-17 09:43:03 +0000

    push from enterprise

--- digisol.agency/forms.py
+++ digisol.agency/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
-
+from wtforms.fields.html5 import EmailField
 
 class ContactForm(FlaskForm):
 	name = StringField('Ім&#39я користувача',
 						   validators=[DataRequired(), Length(min=2, max=20)])
-	email = StringField('Email',
+	email = EmailField('Email',
 						validators=[InputRequired(), Email('please input valid email')])
 	message = TextAreaField('Повідомлення', validators=[DataRequired()])
 
